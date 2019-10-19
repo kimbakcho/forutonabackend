85e1bb089f5cd1f1e0f6986d17da40f032000552
beckho <vngkgk624@gmail.com>
2019-10-19 17:44:21 +0900

    초기 Init 셋팅

---
+++ src/main/java/com/wing/forutona/ForutonaApplication.java
@@ -0,0 +1,13 @@
+package com.wing.forutona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForutonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForutonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wing/forutona/ForutonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wing.forutona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForutonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
